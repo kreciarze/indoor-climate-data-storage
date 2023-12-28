1addd49b417ae9e3dc0419f39bdd83f4763cfa5a
Piotr Karaś <piotr.karas@spoton.com>
2023-12-28 19:05:14 +0100

    fix: Added schema support v2

--- src/db/models/base.py
+++ src/db/models/base.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
-from sqlalchemy import Column, DateTime, func
+from sqlalchemy import Column, DateTime, func, MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+from settings import settings
+
+metadata_obj = MetaData(schema=settings.postgres.db_schema)
 
 
 class Base(DeclarativeBase):
     __name__: str
+    metadata = metadata_obj
 
     @declared_attr.directive
     def __tablename__(cls: Base) -> str:
